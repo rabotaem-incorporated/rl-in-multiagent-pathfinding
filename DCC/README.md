c5afbc6a29e58aa4f6b8ab033250386ed0621b04
justermak <andreiyaremenko9@gmail.com>
2024-12-01 20:02:00 +0300

    added DCC and visualizer

---
+++ DCC/README.md
@@ -0,0 +1,2 @@
+# DCC
+Original code and trained model for paper ['Learning Selective Communication for Multi-Agent Path Finding'](https://arxiv.org/abs/2109.05413).
---
+++ DCC/test.py
@@ -0,0 +1,252 @@
+"""create test set and test model"""
+
+import config as config
+from model import Network
+from environment import Environment
+import torch.multiprocessing as mp
+import torch
+import numpy as np
+from tqdm import tqdm
+import os
+import random
+import pickle
+from typing import Tuple, Union
+import warnings
+import sys
+
+sys.path.append(os.getcwd())
+from test_visualizer.animate import animate
+import datetime
+
+warnings.simplefilter("ignore", UserWarning)
+
+torch.manual_seed(config.test_seed)
+np.random.seed(config.test_seed)
+random.seed(config.test_seed)
+DEVICE = torch.device("cpu")
+torch.set_num_threads(1)
+
+
+def create_test(
+    test_env_settings: Tuple = config.test_env_settings,
+    num_test_cases: int = config.num_test_cases,
+):
+    """
+    create test set
+    """
+
+    for map_length, num_agents, density in test_env_settings:
+
+        name = f"./test_set/{map_length}length_{num_agents}agents_{density}density.pth"
+        print(f"-----{map_length}length {num_agents}agents {density}density-----")
+
+        tests = []
+
+        env = Environment(
+            fix_density=density, num_agents=num_agents, map_length=map_length
+        )
+
+        for _ in tqdm(range(num_test_cases)):
+            tests.append(
+                (np.copy(env.map), np.copy(env.agents_pos), np.copy(env.goals_pos))
+            )
+            env.reset(num_agents=num_agents, map_length=map_length)
+
+        with open(name, "wb") as f:
+            pickle.dump(tests, f)
+
+
+def test_model(
+    model_range: Union[int, tuple], test_set: Tuple = config.test_env_settings
+):
+    """
+    test model in 'saved_models' folder
+    """
+    network = Network()
+    network.eval()
+    network.to(DEVICE)
+
+    pool = mp.Pool(mp.cpu_count() // 2)
+
+    if isinstance(model_range, int):
+        state_dict = torch.load(
+            os.path.join(config.save_path, f"{model_range}.pth"),
+            map_location=DEVICE,
+            weights_only=False,
+        )
+        network.load_state_dict(state_dict)
+        network.eval()
+        network.share_memory()
+
+        print(f"----------test model {model_range}----------")
+
+        for case in test_set:
+            print(f"test set: {case[0]} length {case[1]} agents {case[2]} density")
+            with open(
+                "./test_set/{}length_{}agents_{}density.pth".format(
+                    case[0], case[1], case[2]
+                ),
+                "rb",
+            ) as f:
+                tests = pickle.load(f)
+
+            tests = [(test, network) for test in tests]
+            ret = pool.map(test_one_case, tests)
+
+            success, steps, num_comm = zip(*ret)
+
+            print("success rate: {:.2f}%".format(sum(success) / len(success) * 100))
+            print("average step: {}".format(sum(steps) / len(steps)))
+            print("communication times: {}".format(sum(num_comm) / len(num_comm)))
+            print()
+
+    elif isinstance(model_range, tuple):
+
+        for model_name in range(
+            model_range[0], model_range[1] + 1, config.save_interval
+        ):
+            state_dict = torch.load(
+                os.path.join(config.save_path, f"{model_name}.pth"), map_location=DEVICE
+            )
+            network.load_state_dict(state_dict)
+            network.eval()
+            network.share_memory()
+
+            print(f"----------test model {model_name}----------")
+
+            for case in test_set:
+                print(f"test set: {case[0]} length {case[1]} agents {case[2]} density")
+                with open(
+                    f"./test_set/{case[0]}length_{case[1]}agents_{case[2]}density.pth",
+                    "rb",
+                ) as f:
+                    tests = pickle.load(f)
+
+                tests = [(test, network) for test in tests]
+                ret = pool.map(test_one_case, tests)
+
+                success, steps, num_comm = zip(*ret)
+
+                print("success rate: {:.2f}%".format(sum(success) / len(success) * 100))
+                print("average step: {}".format(sum(steps) / len(steps)))
+                print("communication times: {}".format(sum(num_comm) / len(num_comm)))
+                print()
+
+            print("\n")
+
+
+def test_one_case(args):
+    env_set, network = args
+    env = Environment()
+    env.load(env_set[0], env_set[1], env_set[2])
+    obs, last_act, pos = env.observe()
+
+    done = False
+    network.reset()
+
+    step = 0
+    num_comm = 0
+    while not done and env.steps < config.max_episode_length:
+        actions, _, _, _, comm_mask = network.step(
+            torch.as_tensor(obs.astype(np.float32)).to(DEVICE),
+            torch.as_tensor(last_act.astype(np.float32)).to(DEVICE),
+            torch.as_tensor(pos.astype(int)),
+        )
+        (obs, last_act, pos), _, done, _ = env.step(actions)
+        step += 1
+        num_comm += np.sum(comm_mask)
+
+    return np.array_equal(env.agents_pos, env.goals_pos), step, num_comm
+
+
+def test_one_case_and_animate(args, path):
+    env_set, network = args
+    env = Environment()
+    env.load(env_set[0], env_set[1], env_set[2])
+    obs, last_act, pos = env.observe()
+
+    done = False
+    network.reset()
+
+    step = 0
+    num_comm = 0
+    map = env.map
+    agents_xy = env.agents_pos
+    targets_xy = env.goals_pos
+    steps = []
+    prev_pos = env.agents_pos
+    while not done and env.steps < config.max_episode_length:
+        actions, _, _, _, comm_mask = network.step(
+            torch.as_tensor(obs.astype(np.float32)).to(DEVICE),
+            torch.as_tensor(last_act.astype(np.float32)).to(DEVICE),
+            torch.as_tensor(pos.astype(int)),
+        )
+        (obs, last_act, pos), _, done, _ = env.step(actions)
+        step += 1
+        num_comm += np.sum(comm_mask)
+        cur_pos = np.copy(env.agents_pos)
+        real_actions = np.zeros_like(actions)
+        delta = cur_pos - prev_pos
+        for i in range(len(actions)):
+            if delta[i][0] == 0 and delta[i][1] == 0:
+                real_actions[i] = 4
+            if delta[i][0] == -1 and delta[i][1] == 0:
+                real_actions[i] = 0
+            if delta[i][0] == 1 and delta[i][1] == 0:
+                real_actions[i] = 1
+            if delta[i][0] == 0 and delta[i][1] == -1:
+                real_actions[i] = 2
+            if delta[i][0] == 0 and delta[i][1] == 1:
+                real_actions[i] = 3
+        prev_pos = cur_pos
+        steps.append(real_actions.tolist())
+    
+    animate(
+        map,
+        agents_xy,
+        targets_xy,
+        np.array(steps),
+        path=path,
+    )
+
+    return np.array_equal(env.agents_pos, env.goals_pos), step, num_comm
+
+
+def code_test():
+    env = Environment()
+    network = Network()
+    network.eval()
+    obs, last_act, pos = env.observe()
+    network.step(
+        torch.as_tensor(obs.astype(np.float32)).to(DEVICE),
+        torch.as_tensor(last_act.astype(np.float32)).to(DEVICE),
+        torch.as_tensor(pos.astype(int)),
+    )
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.join(os.getcwd(), "DCC"))
+    # load trained model and reproduce results in paper
+    # test_model(128000)
+    network = Network()
+    network.eval()
+    network.to(DEVICE)
+    state_dict = torch.load(
+            os.path.join(config.save_path, "128000.pth"),
+            map_location=DEVICE,
+            weights_only=False,
+        )
+    network.load_state_dict(state_dict)
+    network.eval()
+    case = [40, 4, 0.3]
+    print(f"test set: {case[0]} length {case[1]} agents {case[2]} density")
+    with open(
+        "./test_set/{}length_{}agents_{}density.pth".format(
+            case[0], case[1], case[2]
+        ),
+        "rb",
+    ) as f:
+        tests = pickle.load(f)
+
+    tests = [(test, network) for test in tests]
+    test_one_case_and_animate(tests[0], path="../test_visualizer/animation.svg")
